01ee7a7730c92ebcd9155e81dcfd631ac07a9e25
CentralLinks17 <109009847+CentralLinks17@users.noreply.github.com>
2024-06-14 16:57:37 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -17,17 +17,19 @@ async def allowed(_, __, message):
 
 @Client.on_message(filters.command(['link', 'plink']) & filters.create(allowed))
 async def gen_link_s(bot, message):
-    vj = await bot.ask(chat_id = message.from_user.id, text = "Now send me your message that you want to store, and I'll provide a shareable link.")
-    file_type = vj.media
+    replied = message.reply_to_message
+    if not replied:
+        return await message.reply('Reply to a message to get a shareable link.')
+    file_type = replied.media
     if file_type not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.AUDIO, enums.MessageMediaType.DOCUMENT]:
-        return await vj.reply("Send me only video,audio,file or document.")
+        return await message.reply("Reply to a supported media")
     if message.has_protected_content and message.chat.id not in ADMINS:
-        return await message.reply("You must be admin to do this !")
-    file_id, ref = unpack_new_file_id((getattr(vj, file_type.value)).file_id)
+        return await message.reply("okDa")
+    file_id, ref = unpack_new_file_id((getattr(replied, file_type.value)).file_id)
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    await message.reply(f"Here is your Link ›› <code>https:///{temp.U_NAME}?start={outstr}</code>")    
+    await message.reply(f"Here is your Link:\n<code>https://t.me/{temp.U_NAME}?start={outstr}</code>")    
     
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
