7c0e73bbbd12388a43f78508b654ff238b364447
CentralLinks17 <109009847+CentralLinks17@users.noreply.github.com>
2024-06-14 19:19:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,8 +84,8 @@ async def start():
 
 if __name__ == '__main__':
     try:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+        # loop = asyncio.new_event_loop()
+        # asyncio.set_event_loop(loop)
         loop.run_until_complete(start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
