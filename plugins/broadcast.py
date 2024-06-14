38e5cc8d7b66bd8c93f8ac98ed08b81330d4d221
CentralLinks17 <109009847+CentralLinks17@users.noreply.github.com>
2024-06-14 09:12:45 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -4,7 +4,7 @@ from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages, broadcast_messages_group
         
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS))
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 async def pm_broadcast(bot, message):
     b_msg = await bot.ask(chat_id = message.from_user.id, text = "Now Send Me Your Broadcast Message")
     try:
@@ -30,6 +30,7 @@ async def pm_broadcast(bot, message):
                     elif sh == "Error":
                         failed += 1
                 done += 1
+                await asyncio.sleep(2)
                 if not done % 20:
                     await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
             else:
@@ -44,12 +45,12 @@ async def pm_broadcast(bot, message):
     except Exception as e:
         print(f"error: {e}")
 
-@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS))
+@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
 async def broadcast_group(bot, message):
     b_msg = await bot.ask(chat_id = message.from_user.id, text = "Now Send Me Your Broadcast Message")
     groups = await db.get_all_chats()
     sts = await message.reply_text(
-        text='Broadcasting your messages To Groups...'
+        text='Broadcasting your messages to groups...'
     )
     start_time = time.time()
     total_groups = await db.total_chat_count()
@@ -61,9 +62,15 @@ async def broadcast_group(bot, message):
         pti, sh = await broadcast_messages_group(int(group['id']), b_msg)
         if pti:
             success += 1
-        elif sh == "Error":
+        elif pti == False:
+            if sh == "Blocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
                 failed += 1
         done += 1
+        await asyncio.sleep(2)
         if not done % 20:
             await sts.edit(f"Broadcast in progress:\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
